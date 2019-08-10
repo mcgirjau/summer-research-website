aadc35af607347d3bf1eb107b3fa3d1a1bc99f49
Maria-Cristiana Girjau <mgirjau21@amherst.edu>
2019-08-09 21:55:54 -0400

    Edits to Code section

--- scripts.js
+++ scripts.js
@@ -21,3 +21,20 @@ $(document).ready(function () {
     });
 });
 /* Scrollbar END */
+
+/* Collapsible code sections BEGIN */
+var collapsible = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < collapsible.length; i++) {
+  collapsible[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var inside = this.nextElementSibling;
+    if (inside.style.maxHeight){
+      inside.style.maxHeight = null;
+    } else {
+      inside.style.maxHeight = inside.scrollHeight + "px";
+    } 
+  });
+}
+/* Collapsible code sections END */
